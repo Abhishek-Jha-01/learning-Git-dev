ac108fca7ba7634b82b9bfd06ac2a11b070c53c5
Eternal86 <Eternalentity86@gmail.com>
2022-10-05 16:43:36 +0530

    added second cpp program(it multiplys two number). File name is B.cpp

---
+++ B.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// multiply two number
+
+int main(void)
+{
+
+    /* code */
+    int a, b;
+    cin >> a >> b;
+    cout << "Multiplication  ans is = " << a * b;
+
+    return 0;
+}
