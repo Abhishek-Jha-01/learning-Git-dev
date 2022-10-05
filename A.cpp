8e9c6b967df12b9be7fe9d9e7c3eddbfd5ef1c0e
Eternal86 <Eternalentity86@gmail.com>
2022-10-05 16:28:39 +0530

    added first cpp program(it adds two number). file name is A

---
+++ A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// add to number
+
+int main(void)
+{
+
+    /* code */
+    int a, b;
+    cin >> a >> b;
+    cout << "sum is = " << a + b;
+
+    return 0;
+}
