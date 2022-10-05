aa1535adede37f5fa9a4c498bf6182f9aff00f5d
Eternal86 <Eternalentity86@gmail.com>
2022-10-05 22:28:24 +0530

    added division function in file b in abhi/divide branch

--- B.cpp
+++ B.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 // multiply two number
+//  also divide after creating a branch
 
 int main(void)
 {
@@ -9,7 +10,17 @@ int main(void)
     /* code */
     int a, b;
     cin >> a >> b;
-    cout << "Multiplication  ans is = " << a * b;
+    cout << "Multiplication  ans is = " << a * b << endl;
+
+    // divide that two number
+    if (b != 0)
+    {
+        cout << "division ans is = " << a / b;
+    }
+    else
+    {
+        cout << "division with 0 not possible";
+    }
 
     return 0;
 }
