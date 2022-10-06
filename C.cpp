cb108fb5f224b6b0e742df63ac9cec739537bc11
Eternal86 <Eternalentity86@gmail.com>
2022-10-06 19:26:54 +0530

    added a third file C (subtracts two number) after pushing file A and B to github

---
+++ C.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+// subract code
+
+int main(void)
+{
+
+    /* code */
+    int a, b;
+    cin >> a >> b;
+    cout << "ans = " << a - b;
+
+    return 0;
+}
